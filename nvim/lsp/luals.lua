7284b35d33045051c9e8bff692c91a5958593616
shakakibara12 <aryan.1843@disroot.org>
2026-04-19 19:51:01 +0530

    update

--- nvim/lsp/luals.lua
+++ nvim/lsp/luals.lua
@@ -1,5 +1,6 @@
-return {
-  cmd = {'lua-language-server'},
-  filetypes = {'lua'},
-  root_markers = {'.luarc.json', '.luarc.jsonc'},
-}
+-- This is just a reference file, for my own sake
+-- return {
+--   cmd = {'lua-language-server'},
+--   filetypes = {'lua'},
+--   root_markers = {'.luarc.json', '.luarc.jsonc'},
+-- }
